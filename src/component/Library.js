cd70bc05c6933cafd1f5d61df52f28da4f97e214
amin-da <darabniya@yahoo.com>
2021-03-13 16:48:39 +0330

    add selected song functionality

--- src/component/Library.js
+++ src/component/Library.js
@@ -1,13 +1,18 @@
 import React from "react";
 import LibrarySong from "./LibrarySong";
 
-const Library = ({ songs }) => {
+const Library = ({ songs, setCurrentSong }) => {
   return (
     <div className="library">
       <h2>Library</h2>
       <div class="library-songs">
         {songs.map((song) => (
-          <LibrarySong song={song} />
+          <LibrarySong
+            song={song}
+            setCurrentSong={setCurrentSong}
+            songs={songs}
+            key={song.id}
+          />
         ))}
       </div>
     </div>
--- src/component/LibrarySong.js
+++ src/component/LibrarySong.js
@@ -1,14 +1,19 @@
 import React from "react";
 
-const LibrarySong = ({ song }) => {
+const LibrarySong = ({ songs, song, setCurrentSong, id }) => {
+  const songSelectHandler = () => {
+    // const selectedSongs = songs.filter((state) => state.id === id);
+    // setCurrentSong(selectedSongs[0]);
+    setCurrentSong(song);
+  };
+
   return (
-    <div className="library-song">
+    <div className="library-song" onClick={songSelectHandler}>
       <img src={song.cover} alt={song.name}></img>
       <div class="song-description">
-      <h3>{song.name}</h3>
-      <h4>{song.artist}</h4>
+        <h3>{song.name}</h3>
+        <h4>{song.artist}</h4>
       </div>
-      
     </div>
   );
 };
